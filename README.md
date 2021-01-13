9c163e6704401eed28640d757747e38d0ddd043f
Jean-Philippe Ehret <jehret@jnesis.com>
2021-01-13 14:39:13 +1300

    1800_TP_HELLO_UNIVERSE_CORRECTION_INTELLIJ

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Repo des Travaux Pratiques ***Devenez développeur Java***
 
-Le cours est disponible [ici sur https://apprendre-java.teachable.com/p/devenez-developpeur-java](https://apprendre-java.teachable.com/p/devenez-developpeur-java).
+Le cours est disponible [ici sur https://apprendre-java.com/p/devenez-developpeur-java](https://apprendre-java.com/p/devenez-developpeur-java).
 
 Chaque étape des travaux pratiques est associée à une branche.
--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,5 +1,10 @@
 public class HelloUniverse {
     public static void main(String... args) {
-        System.out.println("Hello Universe!");
+        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ");
+        int nbPlanetes=8;
+        System.out.println(nbPlanetes);
+        System.out.println("Il y a quelques années cependant, elles étaient au nombre de : ");
+        nbPlanetes=9;
+        System.out.println(nbPlanetes);
     }
 }
