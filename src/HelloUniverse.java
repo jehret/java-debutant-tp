a18aa297d34aad681c1e7e646cb85fd0aaf8d77f
Jean-Philippe Ehret <jehret@jnesis.com>
2021-02-05 19:16:11 +1300

    2900_TP_HELLO_UNIVERSE_CORRECTION_INTELLIJ

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,12 +1,10 @@
 public class HelloUniverse {
     public static void main(String... args) {
         String phraseIntroduction="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
-        System.out.println(phraseIntroduction);
         int nbPlanetes=8;
+        System.out.println(phraseIntroduction+nbPlanetes);
         phraseIntroduction="Il y a quelques années cependant, elles étaient au nombre de : ";
-        System.out.println(nbPlanetes);
-        System.out.println(phraseIntroduction);
         nbPlanetes++;
-        System.out.println(nbPlanetes);
+        System.out.println(phraseIntroduction+nbPlanetes);
     }
 }
