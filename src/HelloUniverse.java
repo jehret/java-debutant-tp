781fcd5821a5de7e3eb1c8777895b6423011f049
Jean-Philippe Ehret <jehret@jnesis.com>
2021-03-01 16:49:06 +1300

    3800_TP_HELLO_UNIVERSE_CORRECTION_INTELLIJ

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,25 +1,19 @@
 public class HelloUniverse {
     public static void main(String... args) {
-        String phraseIntroduction="En %d, les planètes du système solaire étaient au nombre de : %d";
-        int nbPlanetes=0;
-        short annee=2120;
-        if (annee<1600 || annee>2020){
-            System.out.printf("Le programme ne peut pas fournir de résultat pour %d",annee);
-        }
-        else {
-            if (annee < 1700) {
-                nbPlanetes = 7;
-            }
-            else if (annee < 1800) {
-                nbPlanetes = 8;
-            }
-            else if (annee < 2006) {
-                nbPlanetes = 9;
-            } else {
-                nbPlanetes = 8;
-            }
-            System.out.printf(phraseIntroduction, annee, nbPlanetes);
-        }
+        int nbPlanetes=15;
 
+        switch (nbPlanetes) {
+            case 7:
+                System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
+                break;
+            case 8:
+                System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
+                break;
+            case 9:
+                System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre à été réduit à 8");
+                break;
+            default:
+                System.out.printf("Le programme ne peut pas fournir de résultat pour %d",nbPlanetes);
+        }
     }
 }
